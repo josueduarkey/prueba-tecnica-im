d99f4c263a9b6504be9d285649b4fbff039fe91f
Josue Garcia <eduardo.garcia@keyinstitute.edu.sv>
2026-06-12 20:15:07 -0600

    adding styles and update main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import App from "./App.tsx";
+import { AuthProvider } from "./context/AuthContext";
+import { ToastProvider } from "./context/ToastContext";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <ToastProvider>
+          <App />
+        </ToastProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
